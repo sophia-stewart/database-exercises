bfc5bf93f0797ba7e6c4ccf9cbb5ee5c95669bca
Sophia Stewart <sophia.marcell.stewart@gmail.com>
2021-09-22 12:27:14 -0500

    Initial commit

---
+++ indexes_exercises.sql
@@ -0,0 +1,21 @@
+-- INDEXES EXERCISES
+
+USE employees;
+
+DESCRIBE departments;
+SHOW CREATE TABLE departments;
+
+DESCRIBE dept_emp;
+SHOW CREATE TABLE dept_emp;
+
+DESCRIBE dept_manager;
+SHOW CREATE TABLE dept_manager;
+
+DESCRIBE employees;
+SHOW CREATE TABLE employees;
+
+DESCRIBE salaries;
+SHOW CREATE TABLE salaries;
+
+DESCRIBE titles;
+SHOW CREATE TABLE titles;
